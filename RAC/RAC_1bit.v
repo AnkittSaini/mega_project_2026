4978ba57bdc59f047ef3b096b61bf98596fdcd5b
ankittsaini <ankitsaini1576734@gmail.com>
2026-09-20 21:50:08 +0530

    RAC 1 bit added

---
+++ RAC/RAC_1bit.v
@@ -0,0 +1,16 @@
+module FA_1bit(
+    input a, b, cin,
+    output sum, cout
+);
+
+    wire s1, c1, c2;
+
+    xor(s1, a, b);
+    xor(sum, s1, cin);
+
+    and(c1, a, b);
+    and(c2, s1, cin);
+
+    or(cout, c1, c2);
+
+endmodule
